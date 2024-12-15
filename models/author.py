c4e81d5cc8a94cd2af1d8791a8eafe517e0e94a5
Brian Mumo <mumobrian2746@gmail.com>
2024-12-15 17:23:33 +0300

    Add __repr__ methods to models for better object representation

--- models/author.py
+++ models/author.py
@@ -1,7 +1,15 @@
 class Author:
     def __init__(self, id, name):
-        self.id = id
-        self.name = name
+        self._id = id
+        self._name = name
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
 
     def __repr__(self):
-        return f'<Author {self.name}>'
+        return f"Author(id={self.id}, name='{self.name}')"
--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,20 @@
 class Magazine:
-    def __init__(self, id, name, category):
-        self.id = id
-        self.name = name
-        self.category = category
+    def __init__(self, id, name, category="Uncategorized"):
+        self._id = id
+        self._name = name
+        self._category = category
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def category(self):
+        return self._category
 
     def __repr__(self):
-        return f'<Magazine {self.name}>'
+        return f"Magazine(id={self.id}, name='{self.name}', category='{self.category}')"
